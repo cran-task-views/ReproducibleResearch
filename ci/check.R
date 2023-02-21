e3d5fbadb1fb7e6d972461f5c72e6a47b961b19f
John Blischak <jdblischak@gmail.com>
2023-02-21 10:55:43 -0500

    Check the version date
    https://github.com/jdblischak/reproducible-research-ctv/blob/0a066b5a5392ce4a297cbc5ee90614c76cca2732/check.R#L30

--- ci/check.R
+++ ci/check.R
@@ -1,9 +1,27 @@
 #!/usr/bin/env Rscript
 
 library("ctv")
+library("rmarkdown")
 
 f <- "ReproducibleResearch.md"
 
+message("=== Reading ctv...")
 read.ctv(f)
+message("=== Rendering ctv...")
 ctv2html(f)
+message("=== Checking packages...")
 check_ctv_packages(f)
+
+message("=== Checking date...")
+today <- Sys.Date()
+suppressWarnings(timezone <- Sys.timezone())
+message(sprintf("It is %s in the timezone %s", today, timezone))
+header <- yaml_front_matter("ReproducibleResearch.md")
+version <- as.Date(header[["version"]])
+if (version != today) {
+  warning(
+    sprintf("The version in ctv is %s\n", version),
+    "***Don't forget to update the version!***\n",
+    immediate. = TRUE
+  )
+}
